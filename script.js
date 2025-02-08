15257166b7d3eacf232e5aae9dffd25f63e47cdf
Md-Manir-hossain <mdmanirhossainb150@gmail.com>
2025-02-08 22:30:47 +0600

    Conditional Statements

---
+++ script.js
@@ -0,0 +1,108 @@
+let number = 458654;
+
+if (number > 0) {
+    console.log ("The Number is positive");
+}
+
+else {
+    console.log ("The number is Negative");
+}
+
+let numbers = 45865470;
+
+if (numbers %2 == 0) {
+    console.log ("The Number is an Even Number");
+}
+
+else {
+    console.log ("The number is an odd Number");
+}
+
+
+// ======== minimum or maximum digit =========
+
+
+
+67>89
+
+if (89>67) {
+    console.log ("89 is a Maximum Number");
+}
+
+else {
+    console.log ("Or 67 is a maximum Number");
+}
+
+
+let numberss = 4730;
+
+if (numberss %5 == 0) {
+    console.log ("The Number is a visible Number");
+}
+
+else {
+    console.log ("The number is a divisible Number");
+}
+
+
+
+// ======== Conditional Statements =========
+
+let marks;
+
+marks = 75;
+
+if (marks >100 || marks <0){
+    console.log ("Invalid Number")
+}
+
+// else if (marks <0){
+//     console.log ("Number should be 1 or more");
+// }
+
+else if (marks >=80) {
+    console.log ("A+");
+}
+
+else if (marks >=70) {
+    console.log ("A");
+}
+
+else if (marks >=60) {
+    console.log ("A-");
+}
+
+else if (marks >=50) {
+    console.log ("B");
+}
+
+else if (marks >=40) {
+    console.log ("C");
+}
+
+
+else if (marks >=33) {
+    console.log ("D");
+}
+
+else {
+    console.log ("F");
+}
+
+// number-1, number-2, number-3
+
+number1 = 250;
+number2 = 56;
+number3 = 525;
+
+if (number1 > number2 && number1 > number3) {
+    console.log ("The maximum Number is Number-1");
+}
+
+else if (number2 > number3 && number2 > number1) {
+    console.log ("The maximum Number is Number-2");
+}
+
+else if (number3 > number1 && number3 > number2) {
+    console.log ("The maximum nuber is number-3")
+}
